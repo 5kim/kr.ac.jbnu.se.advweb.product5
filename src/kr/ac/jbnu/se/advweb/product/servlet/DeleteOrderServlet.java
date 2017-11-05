14b21f5e7d1e6616c3a63e66f1b1fda78006808f
HongJG <rlwns012@gmail.com>
2017-11-06 08:19:43 +0900

    상품 삭제를 위한 서블릿 작성
    재고 관리에서 상품 삭제

---
+++ src/kr/ac/jbnu/se/advweb/product/servlet/DeleteOrderServlet.java
@@ -0,0 +1,59 @@
+package kr.ac.jbnu.se.advweb.product.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.ac.jbnu.se.advweb.product.utils.DBUtils;
+import kr.ac.jbnu.se.advweb.product.utils.MyUtils;
+
+/**
+ * Servlet implementation class DeleteOrderServlet
+ */
+@WebServlet("/DeleteOrderServlet")
+public class DeleteOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteOrderServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//1 상품의 상품 번호를 가지고 온다.
+		String productNumber = request.getParameter("productNumber");
+		//2. 상품 번호를 이용하여 DB에서 삭제한다.
+		Connection conn = MyUtils.getStoredConnection(request);
+		try {
+			DBUtils.deleteProduct(conn, productNumber);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//3. 다시 재고 화면으로 돌아간다.
+		response.sendRedirect(request.getContextPath() + "/productList");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
